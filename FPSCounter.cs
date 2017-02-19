9b72dddc177d627b105c4b29d0cc348a0218dec8
Sacristan <girts.kesteris@gmail.com>
2017-02-19 17:39:36 +0200

    aded FPSCounter and RandomUtils

---
+++ FPSCounter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Sacristan.Utils
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        private const float UpdateInterval = 0.1f;
+
+        #region Calc vars
+        private float accum = 0.0f;
+        private int frames = 0;
+        private float timeleft;
+        private int qty;
+        private float fps;
+        private float avgFps;
+        #endregion
+
+        void Update()
+        {
+            timeleft -= Time.deltaTime;
+            accum += Time.timeScale / Time.deltaTime;
+            ++frames;
+
+            if (timeleft <= 0.0)
+            {
+                fps = (accum / frames);
+                timeleft = UpdateInterval;
+                accum = 0f;
+                frames = 0;
+            }
+
+            qty++;
+
+            avgFps += (fps - avgFps) / qty;
+        }
+
+        void OnGUI()
+        {
+            GUI.Label(new Rect(Screen.width - 150, 0, 150, 20), "FPS: " + fps.ToString("f2"));
+            GUI.Label(new Rect(Screen.width - 150, 20, 150, 20), "Avg FPS: " + avgFps.ToString("f2"));
+        }
+
+    }
+}
---
+++ RandomUtils.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sacristan.Utils
+{
+    public static class RandomUtils
+    {
+        public class RandomCountBiggerThanRange : Exception
+        {
+            private int min;
+            private int max;
+            private int count; 
+
+            private int Range { get { return max - min; } }
+
+            public RandomCountBiggerThanRange(int count, int min, int max)
+            {
+                this.count = count;
+                this.min = min;
+                this.max = max;
+            }
+
+            public override string Message
+            {
+                get
+                {
+                    return string.Format("Count {0} was greater than assigned min {1} max {2} range {3}", this.count, this.min, this.max, Range);
+                }
+            }
+        }
+
+        public static int[] GenerateRandom(int count)
+        {
+            System.Random random = new System.Random();
+
+            // generate count random values.
+            HashSet<int> candidates = new HashSet<int>();
+            while (candidates.Count < count)
+            {
+                // May strike a duplicate.
+                candidates.Add(random.Next());
+            }
+
+            // load them in to a list.
+            List<int> result = new List<int>();
+            result.AddRange(candidates);
+
+            // shuffle the results:
+            int i = result.Count;
+            while (i > 1)
+            {
+                i--;
+                int k = random.Next(i + 1);
+                int value = result[k];
+                result[k] = result[i];
+                result[i] = value;
+            }
+            return result.ToArray();
+        }
+
+        public static int[] GenerateNonRepeatingRandomIndexSequence(int count, int min=0, int max=100)
+        {
+            System.Random random = new System.Random();
+
+            int diff = max - min;
+            if (diff <= count) throw new RandomCountBiggerThanRange(count, min, max);
+
+            List<int> result = new List<int>();
+
+            while (result.Count < count)
+            {
+                int generatedIndex = random.Next(min, max-1);
+                if (!result.Contains(generatedIndex)) result.Add(generatedIndex);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
