15018a2f0966f9eca4de1b5118e6786b138063df
Sacristan <girts.kesteris@gmail.com>
2017-04-12 17:01:32 +0300

    Added GameObjectUtils

---
+++ GameObjectUtils.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Sacristan.Utils
+{
+    public static class GameObjectUtils
+    {
+        public static void SetLayerRecursively(GameObject obj, int layer)
+        {
+            obj.layer = layer;
+
+            for (int i = 0; i < obj.transform.childCount; i++)
+            {
+                Transform child = obj.transform.GetChild(i);
+                SetLayerRecursively(child.gameObject, layer);
+            }
+        }
+    }
+}
