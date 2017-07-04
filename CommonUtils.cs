4dc5362c98fb48afa1bf839278ddc99bc5d2fc6d
Sacristan <girts.kesteris@gmail.com>
2017-07-04 16:28:34 +0300

    added canvas scaler util

---
+++ CommonUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sacristan.Utils
+{
+    public static class CommonUtils
+    {
+        //public static void EnableComponents(Component[] components, bool flag)
+        //{
+        //    for (int i = 0; i < components.Length; i++)
+        //    {
+        //        components[i].enabled = flag;
+        //    }
+        //}
+
+        public static void UpdateRenderersColor(Renderer[] renderers, Color color)
+        {
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                renderers[i].material.color = color;
+            }
+        }
+    }
+}
