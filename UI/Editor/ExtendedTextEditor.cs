c3eba721e91ac8b5a486bfc292bdd6d6e058c414
Sacristan <girts.kesteris@gmail.com>
2017-08-07 14:49:49 +0300

    added extended text

---
+++ UI/Editor/ExtendedTextEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Sacristan.Utils.UI
+{
+    [CustomEditor(typeof(ExtendedText))]
+    public class ExtendedTextEditor : Editor
+    {
+        private ExtendedText myTarget;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            myTarget = (ExtendedText)target;
+            myTarget.UpdateUnderline();
+        }
+    }
+}
