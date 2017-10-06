42d17e2f7b7096e440452343c719bac9ac0348f7
Sacristan <girts.kesteris@gmail.com>
2017-10-06 15:46:11 +0300

    added date utils

---
+++ DateUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sacristan.Utils
+{
+    public static class DateUtils
+    {
+        public static long GetCurrentUnixTime()
+        {
+            return GetUnixTime(DateTime.Now);
+        }
+
+        public static long GetCurrentUTCUnixTime()
+        {
+            return GetUnixTime(DateTime.UtcNow);
+        }
+
+        public static long GetUnixTime(DateTime dateTime)
+        {
+            DateTime epoch = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+            return (long) (dateTime - epoch).TotalSeconds;
+        }
+    }
+}
