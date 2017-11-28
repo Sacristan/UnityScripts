992111027a7215ce1aaba8265a4ea81f8ac69cd9
Sacristan <girts.kesteris@gmail.com>
2017-11-28 08:39:23 +0200

    added transform utils

---
+++ TransformUtils.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class TransformUtils
+{
+    public static Transform FindChildDeep(this Transform parent, string name)
+    {
+        Transform result = parent.Find(name);
+
+        if (result != null) return result;
+
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+            result = child.FindChildDeep(name);
+
+            if (result != null) return result;
+        }
+
+        return null;
+    }
+}
