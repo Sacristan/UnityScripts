1b65d78905554af8a0f938bf50a780aeee7493d6
Sacristan <girts.kesteris@gmail.com>
2020-04-28 15:48:36 +0300

    add minmax / safe structs

---
+++ Structures/MinMax.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MinMax
+{
+    [System.Serializable]
+    public class Float
+    {
+        [SerializeField] private float min;
+        [SerializeField] private float max;
+
+        public float Min => min;
+        public float Max => max;
+
+        public float GetRandomValue()
+        {
+            return Random.Range(min, max);
+        }
+
+        public float GetValueFromPerc(float perc)
+        {
+            return Mathf.Lerp(min, max, perc);
+        }
+
+        public float GetPercFromValue(float value)
+        {
+            return Mathf.InverseLerp(min, max, value);
+        }
+    }
+
+    [System.Serializable]
+    public class Int
+    {
+        [SerializeField] private int min;
+        [SerializeField] private int max;
+        public int Min => min;
+        public int Max => max;
+
+        public int GetRandomValue()
+        {
+            return Random.Range(min, max);
+        }
+
+        public int GetValueFromPerc(float perc)
+        {
+            return (int)Mathf.Lerp(min, max, perc);
+        }
+
+        public float GetPercFromValue(int value)
+        {
+            return Mathf.InverseLerp(min, max, value);
+        }
+    }
+}
