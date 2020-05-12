526d89159c7495c334169a799185cf798eba728a
Sacristan <girts.kesteris@gmail.com>
2020-05-12 19:17:06 +0300

    add random sfx

---
+++ Structures/RandomSFX.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class RandomSFX
+{
+    [SerializeField] private AudioClip[] sfxes = new AudioClip[0];
+
+    public bool HasAnythingToPlay => sfxes == null || sfxes.Length > 0;
+
+    public AudioClip GetRandomAudioClip()
+    {
+        if (sfxes == null) return null;
+
+        switch (sfxes.Length)
+        {
+            case 0:
+                return null;
+            case 1:
+                return sfxes[0];
+            default:
+                int index = Random.Range(0, sfxes.Length);
+                return sfxes[index];
+        }
+    }
+}
