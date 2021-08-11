a0bd325ce719d209dc80a7c39906ca879364b3dd
Sacristan <girts.kesteris@gmail.com>
2021-08-11 13:12:54 +0300

    make Safe base generic

--- Structures/Safe.cs
+++ Structures/Safe.cs
@@ -1,99 +1,57 @@
 using System;
 
-namespace Safe
+namespace Sacristan.Utils.Safe
 {
-    public static class Functions
+    public abstract class Safe<T> where T : struct, IComparable<T>
     {
         private const int RandomRange = 1000;
+        int offset;
+        T value;
 
-        public static int RandomOffset()
+        public Safe(T value)
+        {
+            offset = RandomOffset();
+            this.value = AddOffset(value, offset);
+        }
+
+        public T GetValue() => SubstractOffset(value, offset);
+
+        public void Reset()
+        {
+            offset = default(int);
+            value = default(T);
+        }
+
+        public override string ToString() => GetValue().ToString();
+        public abstract T AddOffset(T a, int b);
+        public abstract T SubstractOffset(T a, int b);
+
+        private static int RandomOffset()
         {
             int offset = 0;
             Random rnd = new Random();
-            
+
             do
             {
                 offset = rnd.Next(-RandomRange, RandomRange);
-            } 
+            }
             while (offset == 0);
 
             return offset;
         }
     }
 
-    public struct Int
+    public class Int : Safe<int>
     {
-        private int offset;
-        private int value;
-
-        public Int(int value = 0)
-        {
-            offset = Functions.RandomOffset();
-            this.value = value + offset;
-        }
-
-        public int GetValue()
-        {
-            return value - offset;
-        }
-
-        public void Dispose()
-        {
-            offset = 0;
-            value = 0;
-        }
-
-        public override string ToString()
-        {
-            return GetValue().ToString();
-        }
-
-        public static Int operator +(Int f1, Int f2)
-        {
-            return new Int(f1.GetValue() + f2.GetValue());
-        }
-
-        public static Int operator -(Int f1, Int f2)
-        {
-            return new Int(f1.GetValue() - f2.GetValue());
-        }
+        public Int(int value) : base(value) { }
+        public override int AddOffset(int a, int b) => a + b;
+        public override int SubstractOffset(int a, int b) => a - b;
     }
 
-    public struct Float
+    public class Float : Safe<float>
     {
-        private int offset;
-        private float value;
-
-        public Float(float value = 0)
-        {
-            offset = Functions.RandomOffset();
-            this.value = value + offset;
-        }
-
-        public float GetValue()
-        {
-            return value - offset;
-        }
-
-        public void Dispose()
-        {
-            offset = 0;
-            value = 0;
-        }
-
-        public override string ToString()
-        {
-            return GetValue().ToString();
-        }
-
-        public static Float operator +(Float f1, Float f2)
-        {
-            return new Float(f1.GetValue() + f2.GetValue());
-        }
-
-        public static Float operator -(Float f1, Float f2)
-        {
-            return new Float(f1.GetValue() - f2.GetValue());
-        }
+        public Float(float value) : base(value) { }
+        public override float AddOffset(float a, int b) => a + b;
+        public override float SubstractOffset(float a, int b) => a - b;
     }
 }
